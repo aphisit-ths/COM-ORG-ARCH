ef0b10f82bcdb6458fa2ff2b3e275d3f062a9b4c
aphisit-ths <apisit.kmitl@gmail.com>
2022-02-21 22:49:44 +0700

    refactor

---
+++ LAB#3/LAB3MONITORADDER/adds.s
@@ -0,0 +1,13 @@
+/* Add two 32-bit numbers together */
+/* Perfume R0 = R1 + R2 */
+
+    .global _start 
+_start:
+    MOV R1,#50 @ Get 50 into R1
+    MOV R2,#60 @ Get 6 into R2
+    ADDS R0,R1,R2 @ Add the two Register, Result in R0
+
+    MOV R7,#1
+    SWI 0
+
+    
