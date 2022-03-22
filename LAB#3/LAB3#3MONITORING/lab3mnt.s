c4b04b1fe539bd90f515a6668919657046214e30
aphisit-ths <apisit.kmitl@gmail.com>
2022-03-22 13:19:28 +0700

    not finished :(

--- LAB#3/LAB3#3MONITORING/lab3mnt.s
+++ LAB#3/LAB3#3MONITORING/lab3mnt.s
@@ -1,15 +1,16 @@
 /*how to use system call 4 to writing a string */
-
     .global _start
 _start:
-    MOV R7,#4       @Syscall number   
-    MOV R0,#1       @Stdout is monitor
-    MOV R2,#19     @str is 19 char long
-    LDR R1, =string @ string located at string:
+    BL _sum
     SWI 0
+	
+_sum:
+	MOV R1,#50 @ Get 50 into R1
+    MOV R2,#60 @ Get 6 into R2
+    ADDS R0,R1,R2 @ Add the two Register, Result in R0
+	MOV PC, LR   @ Return
 _exit:
     MOV R7,#1       @ exit syscall
     SWI 0 
-
     .data
-string: .ascii "63015208 Aphisit \n"
+string: .ascii "hello world \n"
